d86f1bb95e5e81c624e252f83b4476ce67736a4b
bryan <bryanfeliciano892@gmail.com>
2022-05-22 10:35:13 -0500

    Show and Read functions added

--- app/Main.hs
+++ app/Main.hs
@@ -12,17 +12,32 @@ data Ptype = Pawn | Knight | Bishop | Rook | Queen | King
 
 showPiece :: Piece -> Char
 showPiece (Piece White Pawn) = 'P'
-showPiece (Piece White Knight) = 'P'
-showPiece (Piece White Bishop) = 'P'
-showPiece (Piece White Rook) = 'P'
-showPiece (Piece White Queen) = 'P'
-showPiece (Piece White King) = 'P'
+showPiece (Piece White Knight) = 'K'
+showPiece (Piece White Bishop) = 'B'
+showPiece (Piece White Rook) = 'R'
+showPiece (Piece White Queen) = 'Q'
+showPiece (Piece White King) = 'K'
 showPiece (Piece Black Pawn) = 'p'
-showPiece (Piece Black Knight) = 'p'
-showPiece (Piece Black Bishop) = 'p'
-showPiece (Piece Black Rook) = 'p'
-showPiece (Piece Black Queen) = 'p'
-showPiece (Piece Black King) = 'p'
+showPiece (Piece Black Knight) = 'k'
+showPiece (Piece Black Bishop) = 'b'
+showPiece (Piece Black Rook) = 'r'
+showPiece (Piece Black Queen) = 'q'
+showPiece (Piece Black King) = 'k'
+
+readPiece :: Char -> Piece
+readPiece 'P' = (Piece White Pawn)
+readPiece 'K' = (Piece White Knight)
+readPiece 'B' =  (Piece White Bishop)
+readPiece 'R' =  (Piece White Rook)
+readPiece 'Q' =  (Piece White Queen)
+readPiece 'K' =  (Piece White King)
+readPiece 'p' =  (Piece Black Pawn) 
+readPiece 'k' = (Piece Black Knight)
+readPiece 'b' = (Piece Black Bishop)
+readPiece 'r' = (Piece Black Rook)
+readPiece 'q' = (Piece Black Queen)
+readPiece 'k' = (Piece Black King)
+
 
 main :: IO ()
 main = someFunc
